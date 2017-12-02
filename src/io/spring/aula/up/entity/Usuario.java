275e1efb48c9fbfef615eabbdbd7196e4d80804a
Pedro H. Gomes <pedrogomesup2@gmail.com>
2017-12-02 17:11:47 -0300

    feature-retornando json como resposta
    Criacao de métodos para serem acessados via requisições GET e POST

--- src/io/spring/aula/up/entity/Usuario.java
+++ src/io/spring/aula/up/entity/Usuario.java
@@ -3,6 +3,17 @@ package io.spring.aula.up.entity;
 public class Usuario {
 
 	private String nome;
+	
+	private int idade;
+	
+	private String email;
+
+	public Usuario (String nome) {
+		this.nome = nome;
+	}
+	
+	public Usuario() {
+	}
 
 	public String getNome() {
 		return nome;
@@ -11,8 +22,21 @@ public class Usuario {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
-	public Usuario (String nome) {
-		this.nome = nome;
+
+	public int getIdade() {
+		return idade;
 	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
 }
--- src/io/spring/aula/up/service/UsuarioService.java
+++ src/io/spring/aula/up/service/UsuarioService.java
@@ -1,5 +1,6 @@
 package io.spring.aula.up.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -9,12 +10,50 @@ import io.spring.aula.up.entity.Usuario;
 @Service
 public class UsuarioService {
 
-	public List<Usuario> listaUsuario(Usuario usuario) {
-		return null;
+	public List<Usuario> listaUsuario(Usuario usuarioAdd) {
+        List<Usuario> usuarioLista = new ArrayList<>();
+
+        Usuario usuario = new Usuario();
+
+        usuario.setEmail("pedrogomesup2@gmail.com");
+        usuario.setIdade(21);
+        usuario.setNome("Pedro");
+
+        usuarioLista.add(usuario);
+
+        usuario = new Usuario();
+
+        usuario.setEmail("fulano@gmail.com");
+        usuario.setIdade(35);
+        usuario.setNome("Fulano de tal");
+
+        usuarioLista.add(usuario);
+        usuarioLista.add(usuarioAdd);
+
+        return usuarioLista;
 	}
 
 	public List<Usuario> listaUsuario() {
-		return null;
+        List<Usuario> usuarioLista = new ArrayList<>();
+
+        Usuario usuario = new Usuario();
+
+        usuario.setEmail("pedrogomesup2@gmail.com");
+        usuario.setIdade(21);
+        usuario.setNome("Pedro");
+
+        usuarioLista.add(usuario);
+
+        usuario = new Usuario();
+
+        usuario.setEmail("jennycandido@gmail.com");
+        usuario.setIdade(18);
+        usuario.setNome("Jenny");
+
+        usuarioLista.add(usuario);
+
+        return usuarioLista;
+
 	}
 
 }
