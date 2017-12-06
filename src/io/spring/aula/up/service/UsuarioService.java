87fd7761249be6e63fd261f3205440149c90b61a
pedro.gomes <pedrogomesup2@gmail.com>
2017-12-06 13:53:28 -0200

    Adicionando repository para usuario

--- src/io/spring/aula/up/service/UsuarioService.java
+++ src/io/spring/aula/up/service/UsuarioService.java
@@ -1,59 +1,26 @@
 package io.spring.aula.up.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import io.spring.aula.up.entity.Usuario;
+import io.spring.aula.up.repository.UsuarioRepository;
 
 @Service
 public class UsuarioService {
 
+	@Autowired
+	UsuarioRepository usuarioRepository;
+	
 	public List<Usuario> listaUsuario(Usuario usuarioAdd) {
-        List<Usuario> usuarioLista = new ArrayList<>();
-
-        Usuario usuario = new Usuario();
-
-        usuario.setEmail("pedrogomesup2@gmail.com");
-        usuario.setIdade(21);
-        usuario.setNome("Pedro");
-
-        usuarioLista.add(usuario);
-
-        usuario = new Usuario();
-
-        usuario.setEmail("fulano@gmail.com");
-        usuario.setIdade(35);
-        usuario.setNome("Fulano de tal");
-
-        usuarioLista.add(usuario);
-        usuarioLista.add(usuarioAdd);
-
-        return usuarioLista;
+        usuarioRepository.save(usuarioAdd);
+		return usuarioRepository.findAll();
 	}
 
 	public List<Usuario> listaUsuario() {
-        List<Usuario> usuarioLista = new ArrayList<>();
-
-        Usuario usuario = new Usuario();
-
-        usuario.setEmail("pedrogomesup2@gmail.com");
-        usuario.setIdade(21);
-        usuario.setNome("Pedro");
-
-        usuarioLista.add(usuario);
-
-        usuario = new Usuario();
-
-        usuario.setEmail("jennycandido@gmail.com");
-        usuario.setIdade(18);
-        usuario.setNome("Jenny");
-
-        usuarioLista.add(usuario);
-
-        return usuarioLista;
-
+        return usuarioRepository.findAll();
 	}
 
 }
