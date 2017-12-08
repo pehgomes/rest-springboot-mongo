4dd99b5445b641019c71c6e016ea788c15d80b12
Pedro H. Gomes <pedrogomesup2@gmail.com>
2017-12-07 23:36:02 -0300

    Utilizando Referenciação de documentos
    Foi criado um documento chamado perfil, que se relaciona com o documento usuario.

---
+++ src/io/spring/aula/up/entity/Perfil.java
@@ -0,0 +1,31 @@
+package io.spring.aula.up.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Perfil {
+	
+	@Id
+	private String id;
+	
+	private String nome;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+
+}
